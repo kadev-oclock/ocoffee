02c0738c56be9fa4845533c54353e37e3d6fe091
kadev-oclock <karimaoudiaa@gmail.com>
2024-06-21 19:02:10 +0200

    update panier/404

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,30 @@
-
-
 function initMenu() {
-document.addEventListener("DOMContentLoaded", () => {
-  const burger = document.querySelector(".burger");
-  const navLinks = document.querySelector(".nav-links");
+  document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const navLinks = document.querySelector(".nav-links");
 
-  burger.addEventListener("click", () => {
-    navLinks.classList.toggle("nav-active");
-    burger.classList.toggle("active");
-  });
+    burger.addEventListener("click", () => {
+      navLinks.classList.toggle("nav-active");
+      burger.classList.toggle("active");
+    });
 
-  // Fermer le menu burger lorsqu'un lien est cliqué
-  navLinks.querySelectorAll("a").forEach((link) => {
-    link.addEventListener("click", () => {
-      navLinks.classList.remove("nav-active");
-      burger.classList.remove("active");
+    // Fermer le menu burger lorsqu'un lien est cliqué
+    navLinks.querySelectorAll("a").forEach((link) => {
+      link.addEventListener("click", () => {
+        navLinks.classList.remove("nav-active");
+        burger.classList.remove("active");
+      });
+    });
+    // 404 page
+    const btn = document.querySelector(".btn");
+    btn.addEventListener("mouseover", () => {
+      btn.style.transform = "scale(1.1)";
+    });
+
+    btn.addEventListener("mouseout", () => {
+      btn.style.transform = "scale(1)";
     });
   });
-})
+  // scripts.js
 }
 initMenu();
